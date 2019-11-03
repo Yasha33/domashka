7a3513897858d15aadb935f952a353958604a906
Yasha Home <yasha3@ukr.net>
2019-11-03 15:09:57 +0200

    add homeWork Lesson 6

---
+++ Lesson 6/lesson 6.js
@@ -0,0 +1,27 @@
+var value = prompt('Значение кратное которому','2');
+	
+
+var maxValue = prompt('До какого числа','10');
+
+
+if (!value || !maxValue)
+	console.log('Отмена');
+else{
+	
+	for(var i=1; i<=maxValue; i++){
+		if(!(i % value) && value != 0){
+    	console.log(i);
+    }
+	
+	}
+
+// через while
+	var count = 1;
+	while(count++ <= maxValue){
+
+		if  (!(count % value) && value != 0) {
+		console.log(count);
+		}
+
+	}
+}
