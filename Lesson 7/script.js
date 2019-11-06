1fc9ac6c5ec96b6dff16d84484002e7b0c8522ef
Yasha (work) <yasha3@ukr.net>
2019-11-06 12:19:03 +0200

    Lesson 7 with correction

--- Lesson 7/script.js
+++ Lesson 7/script.js
@@ -1,27 +1,22 @@
 
-
+								//Выводит все в console
 var amountWords = 10,   //количество строк
-	minLetter = 2,		// минимальное количество букв в строке
+	minLetter = 1,		// минимальное количество букв в строке
 	maxLetter = 8,		// максимальное количество букв в строке
     masLeters = [],
     masWords = [],
     amountLetter=0,
     myObject = {};
 
-   // a-z in Unicode 97-122
-   //String.fromCharCode(97,...)
-
-   //var amountLetter =  Math.floor(Math.random() * ((maxLetter + 1) - minLetter) + minLetter);
-
 
    function randomLatter (min = 97, max = 122){
 
-   	return String.fromCharCode ( Math.floor(Math.random() * ((max + 1) - min) + min) );
+   	return String.fromCharCode ( Math.floor(Math.random() * ((max + 1) - min) + min) ); //возвращвет рандомную букву
    }
 
    function randomAmountLatter (){
 
-   	return Math.floor(Math.random() * ((maxLetter + 1) - minLetter) + minLetter)
+   	return Math.floor(Math.random() * ((maxLetter + 1) - minLetter) + minLetter) 
 }
 
   
@@ -29,35 +24,28 @@ var amountWords = 10,   //количество строк
 
   		masLeters[words] = [];
   		
+  		amountLetter = randomAmountLatter();
 
-  		for (var count = 0, amountLetter = randomAmountLatter(); count < amountLetter; count++){
+  		for (var count = 0; count < amountLetter; count++){
 
-  			masLeters[words][count] = randomLatter();
+  			masLeters[words][count] = randomLatter();  // Заполняем массив рандомными буквами
   		}
   	}
 
 
   	masWords = masLeters.map( (value) => value.join('')); // Массив Строк
 
-  	console.log (masWords);
+  	//console.log (masWords);
 
 
-  	//console.log (masWords.map((item) => item.length).filter((value, index, array) => value  ));
-
-/*
-  	for (var value of masWords.map((item) => item.length)) {
-
-  	myObject[value]=[];
-  }*/
-
-  for (var value of masWords) {
+  for (var value of masWords) {			
 
   		if (myObject[value.length] == undefined){
   			myObject[value.length]=[];
   			myObject[value.length].push(value);
   		}
   		else
-  		myObject[value.length].push(value);
+  			myObject[value.length].push(value);
   }
 
   console.log (myObject);
