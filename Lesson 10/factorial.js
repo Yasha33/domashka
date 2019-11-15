ed4f92e9c7f755a9b4be496db0339efd05fc00c0
Yasha (work) <yasha3@ukr.net>
2019-11-15 13:30:28 +0200

    add Lesson 10

---
+++ Lesson 10/factorial.js
@@ -0,0 +1,14 @@
+﻿
+var number = 5;
+
+
+console.log(factorial(number));
+
+
+
+function factorial(number, sum = 1) {
+
+	sum *= number--;
+	return (!number ?  sum : factorial(number,sum));
+
+}
---
+++ Lesson 10/labirint.js
@@ -0,0 +1,29 @@
+﻿
+var labScheme = {
+	'NO': ['###', '###', '###'],
+	'LR': ['###', '   ', '###'],
+	'LT': ['# #', '  #', '###'],
+	'BR': ['###', '#  ', '# #'],
+	'LB': ['###', '  #', '# #'],
+	'TR': ['# #', '#  ', '###']
+};
+
+render([
+	['NO', 'BR', 'LR', 'LB', 'NO'],
+	['LR', 'LT', 'NO', 'TR', 'LR']
+]);
+
+function render(mas) {
+
+	var str = '';
+
+	mas.forEach(element => {
+		for (var i = 0; i < 3; i++) {
+			element.forEach(key => str += labScheme[key][i]);
+			str += '\n';
+		}
+	});
+	console.log(str);
+}
+
+
