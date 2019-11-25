03fe49fc19fa6837e5b9d34bc3c2792cc5f5e4db
Yasha (work) <yasha3@ukr.net>
2019-11-25 15:44:17 +0200

    add button on Lesson 12

--- Lesson 12/script.js
+++ Lesson 12/script.js
@@ -1,19 +1,28 @@
 var obj = {
     circle: document.querySelector('.circle'),
     position: 1,
-    increase: function(value) {
+    increase: function (value) {
         this.position += value;
         this.circle.style.transform = `scale(${this.position})`;
     },
-    decrease: function(value) {
+    decrease: function (value) {
         this.position -= value;
         this.circle.style.transform = `scale(${this.position})`;
     }
 };
 obj.increase(0.2);
-setTimeout(function() {
+setTimeout(function () {
     obj.increase(1);
-    setTimeout(function() {
-        obj.decrease(2);
+    setTimeout(function () {
+        obj.decrease(1.5);
     }, 1000);
 }, 1000);
+
+
+document.querySelector('#up').addEventListener('click', function () {
+    obj.increase(0.5);
+})
+
+document.querySelector('#down').addEventListener('click', function () {
+    obj.decrease(0.5);
+})
