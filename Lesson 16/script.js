bf3baafcf6a1fc152c60e07be38d8ccbcf9435e1
Yasha (work) <yasha3@ukr.net>
2019-12-09 12:31:19 +0200

    add Lesson 16

---
+++ Lesson 16/script.js
@@ -0,0 +1,18 @@
+var croco = new CrocoTurtle(50);
+var bear = new MonsterBear(2);
+var pirate = new Pirate (5);
+var snake = new Snake (1);
+var ghost = new Ghost (3);
+var dragon = new Dragon (4);
+var frankenstein = new Frankenstein (10);
+
+var person = new MainUnit (100);
+
+croco.attack();
+bear.attack();
+pirate.attack();
+snake.attack();
+ghost.attack();
+dragon.attack();
+frankenstein.attack();
+person.move();
